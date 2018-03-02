1ea8705a3c16927ea2ca5df9e220f92d38569555
tdoroshenko <tetyanadoroshenko@gmail.com>
2018-03-02 15:36:49 +0200

    init

--- src/sevenDisplay/sevenDisplay.java
+++ src/sevenDisplay/sevenDisplay.java
@@ -4,61 +4,61 @@ import java.net.SocketPermission;
 
 class sevenDisplay {
     public static void main(String[] args) {
-        int a = 785;
+        int a = 1026;
         String aa = a + "";
         for(int i = 0; i < aa.length(); i++) {
             int k = Character.digit(aa.charAt(i),10);
             switch(k) {
-                case 0:
-                    System.out.println("._.");
-                    System.out.println("|.|");
-                    System.out.println("|_|");
-                    break;
-                case 1:
-                    System.out.println("...");
-                    System.out.println("..|");
-                    System.out.println("..|");
-                    break;
-                case 2:
-                    System.out.println("._.");
-                    System.out.println("._|");
-                    System.out.println("|_.");
-                    break;
-                case 3:
-                    System.out.println("._.");
-                    System.out.println("._|");
-                    System.out.println("._|");
-                    break;
-                case 4:
-                    System.out.println("...");
-                    System.out.println("|_|");
-                    System.out.println("..|");
-                    break;
-                case 5:
-                    System.out.println("._.");
-                    System.out.println("|_.");
-                    System.out.println("._|");
-                    break;
-                case 6:
-                    System.out.println("._.");
-                    System.out.println("|_.");
-                    System.out.println("|_|");
-                    break;
-                case 7:
-                    System.out.println("._.");
-                    System.out.println("..|");
-                    System.out.println("..|");
-                    break;
-                case 8:
-                    System.out.println("._.");
-                    System.out.println("|_|");
-                    System.out.println("|_|");
-                    break;
-                case 9:
-                    System.out.println("._.");
-                    System.out.println("|_|");
-                    System.out.println("._|");
-                    break;
+                    case 0:
+                        System.out.println("._.");
+                        System.out.println("|.|");
+                        System.out.println("|_|");
+                        break;
+                    case 1:
+                        System.out.println("...");
+                        System.out.println("..|");
+                        System.out.println("..|");
+                        break;
+                    case 2:
+                        System.out.println("._.");
+                        System.out.println("._|");
+                        System.out.println("|_.");
+                        break;
+                    case 3:
+                        System.out.println("._.");
+                        System.out.println("._|");
+                        System.out.println("._|");
+                        break;
+                    case 4:
+                        System.out.println("...");
+                        System.out.println("|_|");
+                        System.out.println("..|");
+                        break;
+                    case 5:
+                        System.out.println("._.");
+                        System.out.println("|_.");
+                        System.out.println("._|");
+                        break;
+                    case 6:
+                        System.out.println("._.");
+                        System.out.println("|_.");
+                        System.out.println("|_|");
+                        break;
+                    case 7:
+                        System.out.println("._.");
+                        System.out.println("..|");
+                        System.out.println("..|");
+                        break;
+                    case 8:
+                        System.out.println("._.");
+                        System.out.println("|_|");
+                        System.out.println("|_|");
+                        break;
+                    case 9:
+                        System.out.println("._.");
+                        System.out.println("|_|");
+                        System.out.println("._|");
+                        break;
             }
         }
     }
---
+++ src/sevenDisplay/sevenDisplay3.java
@@ -0,0 +1,87 @@
+package sevenDisplay;
+
+class sevenDisplay3 {
+    public static void main(String[] args) {
+        int a = 140;
+        String one = "._. ";
+        String two = "... ";
+        String three = "|.| ";
+        String four = "..| ";
+        String five = "._| ";
+        String six = "|_| ";
+        String seven = "|_. ";
+        String aa = a + "";
+        for(int i = 0; i < aa.length(); i++) {
+            int k = Character.digit(aa.charAt(i),10);
+            switch(k) {
+                case 0:
+                case 2:
+                case 3:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                        System.out.print(one);
+                        break;
+                        case 1:
+                case 4:
+                        System.out.print(two);
+                        break;
+            }
+        }
+                        System.out.println();
+                         String bb = a + "";
+                        for(int i = 0; i < bb.length(); i++) {
+                        int c = Character.digit(bb.charAt(i),10);
+                switch(c) {
+                    case 0:
+                        System.out.print(three);
+                        break;
+                    case 1:
+                    case 7:
+                        System.out.print(four);
+                        break;
+                    case 2:
+                    case 3:
+                        System.out.print(five);
+                        break;
+                    case 4:
+                    case 8:
+                    case 9:
+                        System.out.print(six);
+                        break;
+                    case 5:
+                    case 6:
+                        System.out.print(seven);
+                        break;
+                }
+            }
+        System.out.println();
+                String cc = a + "";
+                for(int i = 0; i < bb.length(); i++) {
+                    int d = Character.digit(bb.charAt(i),10);
+                    switch(d) {
+                        case 0:
+                        case 6:
+                        case 8:
+                            System.out.print(six);
+                            break;
+                        case 1:
+                        case 4:
+                        case 7:
+                            System.out.print(four);
+                            break;
+                        case 2:
+                            System.out.print(seven);
+                            break;
+                        case 3:
+                        case 5:
+                        case 9:
+                            System.out.print(five);
+                            break;
+                    }
+        }
+    }
+}
+
